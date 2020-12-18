3fac57c689bb4623e16c7b4e0155d2404d1c31a4
Eugene Tcheby <68718382+etcheby@users.noreply.github.com>
2020-12-18 11:47:02 -0500

    Update tgw.tf

--- tgw.tf
+++ tgw.tf
@@ -19,7 +19,7 @@ resource "aws_ec2_transit_gateway" "transit_gateway" {
 
 # Attach Security VPC to the TGW
 resource "aws_ec2_transit_gateway_vpc_attachment" "security_attachment" {
-  subnet_ids         = ["${var.tgw_subnet1.id}","${var.tgw_subnet2.id}"]
+  subnet_ids         = ["${var.tgw_subnet1}","${var.tgw_subnet2}"]
   transit_gateway_id = aws_ec2_transit_gateway.transit_gateway.id
   vpc_id             = var.geocluster_vpc
   transit_gateway_default_route_table_association = false
